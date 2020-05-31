dcdb5d79cccb0d592af5356038847a8b606e0078
gbanfi3 <gbanfi3@gmail.com>
2020-05-31 15:31:08 +0200

    második program

---
+++ 01/a02.py
@@ -0,0 +1,5 @@
+print("hello world")
+name = input("what is your name? ")
+print("neved hossza: " + str(len(name)))
+age = input("hány éves vagy? ")
+print("egy év múlva " + str(int(age)+1) + " éves leszel")
