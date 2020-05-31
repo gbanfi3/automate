dbdea8ce83ea8eb8873cad5b45857004e6019b34
gbanfi3 <gbanfi3@gmail.com>
2020-05-31 18:10:36 +0200

    kicommenteztem a régebbi sorokat

--- 01/a04_while.py
+++ 01/a04_while.py
@@ -1,4 +1,12 @@
-spam = 0
-while spam<5:
-    print("hello " + str(spam))
-    spam = spam+1
+# spam = 0
+# while spam<5:
+#     print("hello " + str(spam))
+#     spam = spam+1
+
+# name = ""
+# while name != "your name":
+#     name = input("please type your name: ")
+# print("thank you")
+
+while True:
+    print("végtelen")
