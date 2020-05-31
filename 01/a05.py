e81dc436838d28489df0a222e3664d4d6341055c
gbanfi3 <gbanfi3@gmail.com>
2020-05-31 18:32:56 +0200

    range

--- 01/a05.py
+++ 01/a05.py
@@ -1,3 +1,9 @@
-print("my name is")
-for i in range(5):
-    print("Jimmy" + str(i))
+# print("my name is")
+# for i in range(5):
+#     print("Jimmy" + str(i))
+
+summ = 0
+for i in range(1,101):
+    print(i)
+    summ += i
+print(summ)
