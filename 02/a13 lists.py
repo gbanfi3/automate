2b07a2c948467ddbe0506570e91d8cee412e07f3
gbanfi3 <gbanfi3@gmail.com>
2020-06-01 16:26:18 +0200

    lists

--- 02/a13 lists.py
+++ 02/a13 lists.py
@@ -1,2 +1,9 @@
-for i in [1,2,3,4,5]:
-    print(i)
+# for i in [1,2,3,4,5]:
+#     print(i)
+
+aa = ['vv', 'kk', 'oo', 'nn']
+for i in range(len(aa)):
+    print(aa[i])    #TODO
+
+a, s, d, f = aa #TODO
+print(a,s,d,f)
