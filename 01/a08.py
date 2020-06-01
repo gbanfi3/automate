0c135e06be5ee8c4d59e46813e80d6b67acf1311
gbanfi3 <gbanfi3@gmail.com>
2020-06-01 13:04:21 +0200

    hello

--- 01/a08.py
+++ 01/a08.py
@@ -1,6 +1,9 @@
-def hello():
-    print("hello")
-    print("hello2")
+def hello(nev):
+    print("hello " + nev)
+    #print("hello2")
 
-for i in range(5):
-    hello()
+# for i in range(5):
+#     hello()
+
+hello("alma")
+hello("körte")
