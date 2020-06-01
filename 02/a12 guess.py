22a23b4aa810ce9e655b63c4a7cdd9949ecb50a6
gbanfi3 <gbanfi3@gmail.com>
2020-06-01 15:29:40 +0200

    utolsó verzió

--- 02/a12 guess.py
+++ 02/a12 guess.py
@@ -3,13 +3,13 @@ nummin = 1
 nummax = 20
 num_guesses = 10
 num = random.randint(nummin, nummax)
+print("ezt kell kitalálni: " + str(num))
 name = input("Hello. What is your name? ")
 print("Well, " + name + ", I am thinking of a number between " + str(nummin) + " and " + str(nummax) + ".")
 for i in range(1, num_guesses+1):
     guess = input("Take a guess: ")
     try:
         guess_num = int(guess)
-        print("ezt kell kitalálni: " + guess)
     except:
         print("nem számot ütöttél be !")
         continue
