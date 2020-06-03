ab76f41864e9c55a911bafb5ca14add5ec8a948a
gbanfi3 <gbanfi3@gmail.com>
2020-06-04 00:05:18 +0200

    regexp

--- 03/a19.py
+++ 03/a19.py
@@ -2,4 +2,21 @@ import re
 
 message = "hivjál fel a 123-456-7890 számon, vagy inkább ezen: 234-567-8901"
 
-phoneNumRegex = re.compile(message)
+phoneNumRegex = re.compile(r'(\d\d\d)-(\d\d\d-\d\d\d\d)')
+mo = phoneNumRegex.search(message)
+print(mo.group(2))
+
+message = "hivjál fel a 123-456-7890 számon, vagy inkább ezen: (234)-567-8901"
+phoneNumRegex = re.compile(r'\(\d\d\d\)-\d\d\d-\d\d\d\d')
+mo = phoneNumRegex.search(message)
+print(mo.group())
+
+message = "a batmobile akármi batss"
+mesRegex = re.compile(r'bat(man|valami|mobile|ss)')
+mo = mesRegex.search(message)
+print(mo.group())
+
+message = "a batw6oman akármi batss"
+mesRegex = re.compile(r'bat(wo)?man')
+mo = mesRegex.search(message)
+print(mo.group())
