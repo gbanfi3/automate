59560cd74da36a645f87e8ac253f0a1e5f73efc9
gbanfi3 <gbanfi3@gmail.com>
2020-06-03 22:07:01 +0200

    string

--- 02/a17 strings.py
+++ 02/a17 strings.py
@@ -1,2 +1,23 @@
+import pyperclip
 spam = "hello world"
 print(spam.upper())
+spam = spam.lower()
+print(spam)
+
+a= '.'.join(['jj', 'oo', 'bb'])
+print(a)
+
+b = 'hello alma kkk itt mi ez'
+b2 = '-'.join(b.split())
+b3 = '='.join(b2.split('-'))
+print(b3)
+
+c= "alma".rjust(10)
+c= "alma".ljust(10, 'h').rjust(22, 'x')
+print(c)
+c= c.strip('hx')
+print(c)
+
+pyperclip.copy = 'hello'
+# d= pyperclip.paste
+# print(d)
