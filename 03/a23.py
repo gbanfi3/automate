cf34fdf6036ac12fe93ea2fe4b0a41df0c25af06
gbanfi3 <gbanfi3@gmail.com>
2020-06-06 16:26:07 +0200

    regexp

---
+++ 03/a23.py
@@ -0,0 +1,43 @@
+#! python3
+
+import re
+
+# TODO: create regex for phone numbers
+
+reg2 = re.compile(r'((\(\d\d\d\) |\d\d\d-)?'  # area code
+                 r'\d\d\d-\d\d\d\d'  # tel num
+                 r' ext\.? \d\d(\d){0-3}|x\d\d(\d){0-3})')    #extension
+reg = re.compile(r'((\(\d\d\d\) |\d\d\d-)?\d\d\d-\d\d\d\d( ext\.? \d\d\d\d\d)?)')
+szoveg = "123-123-0000, 555-0000, (415) 555-0000, 555-0001 ext. 12345, ext 12345, x12345"
+lista = reg.findall(szoveg)
+print(lista)
+
+
+# TODO: create regex for email addresses
+
+
+
+# TODO: create regex for phone numbers
+
+
+
+# TODO: create regex for phone numbers
+
+
+
+# TODO: create regex for phone numbers
+
+
+
+# TODO: create regex for phone numbers
+
+
+
+# TODO: create regex for phone numbers
+
+
+
+# TODO: create regex for phone numbers
+
+
+
