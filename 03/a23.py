925976f3abcc9fbd87bb84dc5d492d32dd0870f4
gbanfi3 <gbanfi3@gmail.com>
2020-06-06 16:28:02 +0200

    regexp

--- 03/a23.py
+++ 03/a23.py
@@ -6,9 +6,9 @@ import re
 
 reg2 = re.compile(r'((\(\d\d\d\) |\d\d\d-)?'  # area code
                  r'\d\d\d-\d\d\d\d'  # tel num
-                 r' ext\.? \d\d(\d){0-3}|x\d\d(\d){0-3})')    #extension
-reg = re.compile(r'((\(\d\d\d\) |\d\d\d-)?\d\d\d-\d\d\d\d( ext\.? \d\d\d\d\d)?)')
-szoveg = "123-123-0000, 555-0000, (415) 555-0000, 555-0001 ext. 12345, ext 12345, x12345"
+                 r' ext\.? \d\d(\d){0,3}|x\d\d(\d){0,3})')    #extension
+reg = re.compile(r'((\(\d\d\d\) |\d\d\d-)?\d\d\d-\d\d\d\d( ext\.? \d\d\d{0,3})?)')
+szoveg = "123-123-0000, 555-0000, (415) 555-0000, 555-0001 ext. 1234, ext 12345, x12345"
 lista = reg.findall(szoveg)
 print(lista)
 
