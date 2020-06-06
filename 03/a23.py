ba98315ce75a10ab3c55130b7df76f524cfebd59
gbanfi3 <gbanfi3@gmail.com>
2020-06-06 16:43:59 +0200

    regexp

--- 03/a23.py
+++ 03/a23.py
@@ -2,18 +2,15 @@
 
 import re
 
-# TODO: create regex for phone numbers
-
-reg2 = re.compile(r'((\(\d\d\d\) |\d\d\d-)?'  # area code
-                 r'\d\d\d-\d\d\d\d'  # tel num
-                 r' ext\.? \d\d(\d){0,3}|x\d\d(\d){0,3})')    #extension
-reg = re.compile(r'(((\(\d\d\d\) |\d\d\d-)?\d\d\d-\d\d\d\d)?( ext\.? \d\d\d{0,3})?)')
+reg = re.compile(r'(((\(\d\d\d\) |\d\d\d-)?'    # area
+                 r'\d\d\d-\d\d\d\d)?'           # tel
+                 r'( ext\.? \d{2,5}|x\d{2,5})?)') #extension
 szoveg = "123-123-0000, 555-0000, (415) 555-0000, 555-0001 ext. 1234, ext 12345, x12345"
 lista = reg.findall(szoveg)
 print(lista)
 for i in range(len(lista)):
-    print(lista[i][0])
-
+    if lista[i][0].strip():
+        print(lista[i][0].strip())
 
 # TODO: create regex for email addresses
 
