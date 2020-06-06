7d09ca95df12f8761fd7bdf34dc91cc32a174098
gbanfi3 <gbanfi3@gmail.com>
2020-06-06 17:08:40 +0200

    végleges

--- 03/a23.py
+++ 03/a23.py
@@ -1,5 +1,4 @@
 #! python3
-
 import re
 
 reg = re.compile(r'(((\(\d\d\d\) |\d\d\d-)?'    # area
@@ -7,12 +6,27 @@ reg = re.compile(r'(((\(\d\d\d\) |\d\d\d-)?'    # area
                  r'( ext\.? \d{2,5}|x\d{2,5})?)') #extension
 szoveg = "123-123-0000, 555-0000, (415) 555-0000, 555-0001 ext. 1234, ext 12345, x12345"
 lista = reg.findall(szoveg)
-print(lista)
+tels = []
 for i in range(len(lista)):
-    if lista[i][0].strip():
-        print(lista[i][0].strip())
+    if lista[i][0]:
+        tels.append(lista[i][0])
+print(tels)
 
 # TODO: create regex for email addresses
+szoveg = "a.b@c.d a_+_.B.c@d_+.e_+ a,b@.."
+reg = re.compile(r""
+                 r"[a-zA-Z0-9.+_]+"
+                 r"@"
+                 r"[a-zA-Z0-9.+_]+"
+                 r"\."
+                 r"[a-zA-Z0-9.+_]+"
+                 r"")
+lista = reg.findall(szoveg)
+emails = []
+for i in range(len(lista)):
+    if lista[i]:
+        emails.append(lista[i])
+print(emails)
 
 
 
