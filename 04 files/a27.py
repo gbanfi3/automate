d412ee682c97f248d344a7b98adc3fc8650ee2a6
gbanfi3 <gbanfi3@gmail.com>
2020-06-08 22:29:46 +0200

    send2trash

--- 04 files/a27.py
+++ 04 files/a27.py
@@ -1,4 +1,5 @@
 import shutil
 # shutil.copy("./aldir/file01.txt", './aldir2')
 # shutil.copy("./aldir/file01.txt", './aldir2/masnev.txt')
-shutil.copytree('./aldir',"./aldir3")
+# shutil.copytree('./aldir',"./aldir3")
+shutil.move('./aldir', './aldir4')
---
+++ 04 files/a28 delete.py
@@ -0,0 +1,7 @@
+import os
+import shutil
+import send2trash
+# os.unlink("./aldir4/aldir2/masnev.txt")
+# os.rmdir("./aldir2")
+# shutil.copytree("./aldir4", "./aldir5")
+send2trash.send2trash("./aldir4")
