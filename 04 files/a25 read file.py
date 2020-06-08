7a83dbe35dc3dcb4c99e086e73668300e03f67b6
gbanfi3 <gbanfi3@gmail.com>
2020-06-08 21:21:33 +0200

    file írás

--- 04 files/a25 read file.py
+++ 04 files/a25 read file.py
@@ -1,5 +1,16 @@
 import os
 
-hellofile = open("./aldir/file01.txt")
-a = hellofile.read()
+# hellofile = open("./aldir/file01.txt")
+# # a = hellofile.read()
+# b = hellofile.readlines()
+# hellofile.close()
+# # print(a)
+# print(b)
+
+hellofile = open("./aldir/file01.txt", "a")
+hellofile.write("\nmégvalami")
+hellofile.close()
+hellofile = open("./aldir/file01.txt", "r")
+a = hellofile.readlines()
+hellofile.close()
 print(a)
