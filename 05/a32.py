fc818f374e6395bba05a2eef2a0c17b3f0e1db3f
gbanfi3 <gbanfi3@gmail.com>
2020-06-09 19:12:09 +0200

    logging into file

--- 05/a32.py
+++ 05/a32.py
@@ -1,2 +1,14 @@
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='./logFile.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def fact(n):
+    i = 1
+    for j in range(1,n+1):
+        i *= j
+        logging.info("j=%s" % (i))
+        if j==1:
+            logging.debug("nagy öröm     j=%s" % (j))
+    return i
+
+for i in range(1,5):
+    print(str(i) + ": " + str(fact(i)))
